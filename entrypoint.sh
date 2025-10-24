767511f7cbff43f7b370beabaa1c27581889cfff
JM Dev <juanma261005@gmail.com>
2025-10-24 18:23:11 -0300

    remove some characters

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,17 @@ DB_HOST="db"
 DB_PORT="3306"
 TIMEOUT=45
 
-echo "Esperando hasta $TIMEOUT segundos a que la base de datos MySQL en $DB_HOST:$DB_PORT est� disponible..."
+echo "Esperando hasta $TIMEOUT segundos a que la base de datos MySQL en $DB_HOST:$DB_PORT este disponible..."
 
 start_time=$(date +%s)
 while ! nc -z $DB_HOST $DB_PORT; do
   current_time=$(date +%s)
   elapsed_time=$((current_time - start_time))
   if [ $elapsed_time -ge $TIMEOUT ]; then
-    echo "Error: La base de datos no est� disponible despu�s de $TIMEOUT segundos. Saliendo."
+    echo "Error: La base de datos no est� disponible despues de $TIMEOUT segundos. Saliendo."
     exit 1
   fi
-  echo "MySQL a�n no est� lista ($elapsed_time segundos transcurridos). Reintentando en 1 segundo..."
+  echo "MySQL aun no esta lista ($elapsed_time segundos transcurridos). Reintentando en 1 segundo..."
   sleep 1
 done
 
@@ -24,9 +24,9 @@ dotnet InventorySystem-webapi.dll --migrate
 
 MIGRATION_EXIT_CODE=$?
 if [ $MIGRATION_EXIT_CODE -ne 0 ]; then
-  echo "ERROR: Fallo al aplicar las migraciones. C�digo de salida: $MIGRATION_EXIT_CODE"
+  echo "ERROR: Fallo al aplicar las migraciones. Codigo de salida: $MIGRATION_EXIT_CODE"
   exit $MIGRATION_EXIT_CODE
 fi
 
-echo "Migraciones aplicadas con �xito. Iniciando la API..."
+echo "Migraciones aplicadas con exito. Iniciando la API..."
 exec dotnet InventorySystem-webapi.dll
